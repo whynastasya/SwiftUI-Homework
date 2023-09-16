4311f33a17b280eb5194e3aaca981574586cbea2
Nastasya Grigorchuk <91945406+whynastasya@users.noreply.github.com>
2023-09-16 17:28:36 +0300

    Initial commit

---
+++ SwiftUI-Homework/SwiftUI-Homework/SwiftUI_HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HomeworkApp.swift
+//  SwiftUI-Homework
+//
+//  Created by nastasya on 16.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
